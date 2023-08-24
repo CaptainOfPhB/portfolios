84d92d2a1aa9fe4a84dd6b0dd8c55a4f98fcdd56
CaptainOfPhB <captainofphb@gmail.com>
2023-08-24 23:39:56 +0800

    chore: move outside of pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import Image from 'next/image';
 
-import config from './config';
+import config from '../config';
 import styles from '../styles/Home.module.css';
 
 function Home() {
