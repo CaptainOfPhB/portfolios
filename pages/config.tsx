6cef57a847315b0b27c5cbb301f11d86e018e25e
CaptainOfPhB <captainofphb@gmail.com>
2023-08-24 23:29:15 +0800

    chore: update

---
+++ pages/config.tsx
@@ -0,0 +1,26 @@
+const config = {
+  siteIcon: '/favicon.ico',
+  siteTitle: 'CaptainOfPhB',
+  siteDescription: 'CaptainOfPhb\'s profile',
+  title: 'CaptainOfPhB',
+  mail: 'captainofphb@gmail.com',
+  description: 'Focus on front-end development, currently in Shanghai, China.',
+  socialLinks: [
+    { title: 'GitHub', icon: '/github.svg', url: 'https://github.com/CaptainOfPhB', width: 32, height: 32 },
+    { title: 'blog', icon: '/blog.svg', url: 'https://daijiangtao.blog', width: 23, height: 23 },
+    { title: 'Twitter', icon: '/twitter.svg', url: 'https://twitter.com/CaptainOfPhB' },
+    { title: '知乎', icon: '/zhihu.svg', url: 'https://www.zhihu.com/people/CaptainInPHW' },
+    { title: 'WeChat', icon: '/wechat.svg', url: 'https://picx.zhimg.com/80/v2-502fce7fabff98705dfab84cc58bb31d_1440w.png' },
+  ],
+  introductions: [
+    `Hi there, I'm CaptainOfPhB, a front-end developer from Shanghai, China.`,
+    `At present, I have ${new Date().getFullYear() - 2018} years of experience on front-end development, and working with React for over ${new Date().getFullYear() - 2018 - 1} years. I have been developing backend management system for most of my work. I write clean and simple code, and always try to find the right way to solve problems in my work.`,
+    `Outside of work, I like to self-driving travel with friends, or sometime cook at home on weekends.`,
+    `Like most of programmer, I have two lovely cats which have been with me over ${new Date().getFullYear() - 2018 - 1} years.`,
+    `I learn English and some new techniques in my spare time. I would appreciate it if you could teach me anything new to me.`,
+    `Feel free to contact me if you can offer me a better job or just wanna make friend with me.`,
+    `Looking forward to your email.`
+  ]
+}
+
+export default config;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,89 +1,39 @@
-import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
+
+import config from './config';
 import styles from '../styles/Home.module.css';
 
-const Home: NextPage = () => {
+function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>CaptainOfPhB</title>
-        <meta name='description' content='CaptainOfPhb profile' />
-        <link rel='icon' href='/favicon.ico' />
+        <title>{config.siteTitle}</title>
+        <link rel='icon' href={config.siteIcon} />
+        <meta name='description' content={config.siteDescription} />
       </Head>
 
       <main className={styles.main}>
         <div className={styles.avatar}>
-          <Image src='/avatar.svg' alt='me' width={256} height={256} />
+          <div className={styles.me} />
         </div>
-        <h1 className={styles.title}>CaptainOfPhB</h1>
-        <p className={styles.description}>Focus on front-end development, currently in Shanghai, China.</p>
+        <div className={styles.title}>{config.title}</div>
         <div className={styles.links}>
-          <a className={styles.link} title='GitHub' target='_blank' rel='noreferrer me' href='https://github.com/CaptainOfPhB'>
-            <Image src='/github.svg' alt='github' width={32} height={32} />
-          </a>
-          <a className={styles.link} title='Blog' target='_blank' rel='noreferrer me' href='https://daijiangtao.blog'>
-            <Image src='/blog.svg' alt='blog' width={24} height={24} />
-          </a>
-          <a className={styles.link} title='resume' target='_blank' rel='noreferrer me' onClick={() => alert('Sorry, resume is under constructing')}>
-            <Image src='/resume.svg' alt='resume' width={26} height={26} />
-          </a>
-          <a className={styles.link} title='知乎' target='_blank' rel='noreferrer me' href='https://www.zhihu.com/people/CaptainInPHW'>
-            <Image src='/zhihu.svg' alt='知乎' width={28} height={28} />
-          </a>
-          <a className={styles.link} title='Twitter' target='_blank' rel='noreferrer me' href='https://twitter.com/CaptainOfPhB'>
-            <Image src='/twitter.svg' alt='twitter' width={28} height={28} />
-          </a>
-          <a className={styles.link} title='Mastodon' target='_blank' rel='noreferrer me' href='https://mastodon.world/@captainofphb'>
-            <Image src='/mastodon.svg' alt='mastodon' width={28} height={28} />
-          </a>
-          <a className={styles.link} title='Phone' target='_blank' rel='noreferrer me' href='tel:+8617349777173'>
-            <Image src='/phone.svg' alt='phone' width={28} height={28} />
-          </a>
-          <a className={styles.link} title='WeChat' target='_blank' rel='noreferrer me' href='https://pic1.zhimg.com/80/v2-b5de1adce0d79ec3bed787f9369db7d9_1440w.jpeg'>
-            <Image src='/wechat.svg' alt='wechat' width={28} height={28} />
-          </a>
+          {config.socialLinks.map(it => (
+            <a key={it.title} className={styles.link} title={it.title} target='_blank' rel='noreferrer me' href={it.url}>
+              <Image src={it.icon} alt={it.title} width={it.width || 28} height={it.height || 28} />
+            </a>
+          ))}
         </div>
         <div className={styles.divider} />
         <div className={styles.introduction}>
-          <p>
-            I&apos;m CaptainOfPhB and my Chinese name is 戴江涛. My hometown is in Tianshui, Gansu province where I have lived for about 20 years. I worked for China Mobile company
-            for one year and a half after graduating from XUT(Xi&apos;an University of Technology) in 2016. I quitted the job when I found I&apos;m really interested about the
-            coding, so I found my first front-end development job in 2018 at Shanghai after learning the front-end development for about a half year.
-          </p>
-          <p>
-            At present, I have 4 years of experience and have been developing with React.js for over 3 years. I&apos;ve been developing backend management systems for most of my
-            work. I try to keep my code clean and simple, and I&apos;m always trying to find a better way to solve problems.
-          </p>
-          <p>
-            I like to traveling by self-driving with my friends outside of work, and sometime I would like to cooking at home. Also, I have two lovely cats who have been with
-            me for three years.
-          </p>
-          <p>I try to learn English and some new techniques in my spare time. I would appreciate it if you could teach me anything new to me. Feel free to contact me.</p>
-          <p>Looking forward to your mail.</p>
+          {config.introductions.map((it, index) => <p key={index}>{it}</p>)}
         </div>
         <a className={styles.email} href='mailto:captainofphb@gmail.com'>
           <Image src='/send.svg' alt='send' width={28} height={28} />
           Email Me
         </a>
       </main>
-
-      <footer className={styles.footer}>
-        <div className={styles.item}>
-          Powered by{' '}
-          <a href='https://nextjs.org' target='_blank' rel='noopener noreferrer'>
-            <span className={styles.logo}>
-              <Image src='/nextjs.svg' alt='Next Logo' width={72} height={16} />
-            </span>
-          </a>
-        </div>
-        <div className={styles.item}>
-          Hosted on{' '}
-          <span className={styles.logo}>
-            <Image src='/vercel.svg' alt='Vercel' width={72} height={16} />
-          </span>
-        </div>
-      </footer>
     </div>
   );
 };
